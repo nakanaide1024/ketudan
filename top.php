3e033897e35105a93cc46f2684cae180bd5636ee
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-27 02:08:07 +0900

    大枠のレイアウト

--- top.php
+++ top.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>決断</title>
 </head>
 <body>
-  
+  <div class="header">
+    <h2>決断</h2>
+  </div>
+  <div class="main"></div>
+  <div class="footer">
+  ©️2021 上りだけ坂 
+  </div>
 </body>
 </html>
