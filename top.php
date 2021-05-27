d813c9356d6c68219b359617adf2122490ff0f28
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-27 18:14:34 +0900

    暫定レイアウト作成

--- top.php
+++ top.php
@@ -8,16 +8,36 @@
   <title>決断</title>
 </head>
 <body>
-  <div class="wrapper">
-    <div class="header" >
-      <h2>決断</h2>
+  <div class="header" >
+    <h2>決断</h2>
+  </div>
+  <div class="main">
+    <div class="content">
+      <div class="message">
+        <div class="q">
+          <p>何をそんなに悩んでいるんだ？</p>
+        </div>
+      </div>
+      <div class="form">
+      <form action="result.php" method="POST">
+        <div class="a">
+          <input type="text" name="a" value="" class="text">
+        </div>
+        <div class="or">
+          <h2>或いは</h2>
+        </div>
+        <div class="b">
+          <input type="text" name="b" value="" class="text">
+        </div>
+      </div>
+      <div class="submit">
+        <input type="submit" value="決断せよ" class="button">
+      </div>
+      </form>
     </div>
-    <div class="main">
-      <p>main</p>
-    </div>
-    <div class="footer">
-    ©️2021 上りだけ坂 
-    </div>
+  </div>
+  <div class="footer">
+  ©️2021 上りだけ坂 
   </div>
 </body>
 </html>
