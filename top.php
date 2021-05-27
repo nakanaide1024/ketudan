bd1505037bd19e3552a2b38fb831d7ab21d00e74
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-27 13:05:29 +0900

    headerの作成

--- top.php
+++ top.php
@@ -4,10 +4,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="top.css">
   <title>決断</title>
 </head>
 <body>
-  <div class="header">
+  <div class="header" style = >
     <h2>決断</h2>
   </div>
   <div class="main"></div>
