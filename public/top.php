e59908c834a65a82da1ef195c406d2b3505c32ea
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-29 13:10:47 +0900

    top.cssのファイル位置変更

--- public/top.php
+++ public/top.php
@@ -14,7 +14,7 @@ unset($_SESSION['err']);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="top.css">
+  <link rel="stylesheet" href="../css/top.css">
   <title>決断</title>
 </head>
 <body>
